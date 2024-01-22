5e54522001461e280ca103d5a1059765d6a654d0
pinguille0027 <guillermo.orio27@gmail.com>
2024-01-22 14:57:37 +0100

    plantilla de aso

---
+++ equipointermedio/engade-ruta.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+#REDE 22.88.0.0/16 por rcurro
+ip r add 22.88.0.0/16 via 192.168.10.254
---
+++ home-laptop-01/engade-ruta.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+#REDE 22.88.0.0/16 por rcasa
+ip r add 22.88.0.0/16 via 192.168.20.254
